1282d42ea7b36ed1d239329d03ba43e41deac230
ChuLiJ <hrow@1241.com>
2025-03-21 13:54:59 +0800

    产生初试虚拟机分布

--- generation_data.py
+++ generation_data.py
@@ -43,6 +43,49 @@ def cluster_vms(vms, n_cluster=3):
     return vms, cluster_centers
 
 
+def generate_requests(time_steps, vms, request_rate=0.3, avg_lifetime=5):
+    requests = []
+    active_vms = {}
+
+    for t in range(time_steps):
+        # 新请求虚拟机
+        if np.random.rand() < request_rate:
+            vm = np.random.choice(vms)
+            lifetime = np.random.exponential(avg_lifetime)
+            requests.append({
+                "time": t,
+                "vm": vm,
+                "type": "start",
+                "lifetime": int(lifetime)
+            })
+            active_vms[vm["Vid"]] = t + lifetime
+        # 移除过期虚拟机
+        expired_vids = [vid for vid, end_time in active_vms.items() if end_time <= t]
+        for vid in expired_vids:
+            requests.append({
+                "time": t,
+                "vm": next(vm for vm in vms if vm["Vid"] == vid),
+                "type": "stop"
+            })
+            del active_vms[vid]
+    return requests
+
+
+def initial_deployment(pms, vms):
+    deployment = {pm['Pid']: [] for pm in pms}
+    dcp = [[0 for _ in range(pms)] for _ in range(vms)]
+    for vm in vms:
+        while True:
+            pm = np.random.choice(pms)
+            used_cpu = sum(v["Cpu"] for v in deployment[pm["Pid"]])
+            used_mem = sum(v['Mem'] for v in deployment[pm["Pid"]])
+            if (used_cpu + vm["Cpu"] <= pm["Cpu"]) and (used_mem + vm["Mem"] <= pm["Mem"]):
+                deployment[pm["Pid"]].append(vm)
+                dcp[vm['Vid']][pm['Pid']] = 1
+                break
+    return deployment, dcp
+
+
 pms = generate_pms(50, (8, 64), (32, 256))
 vms = generate_vms(200, 64, 256)
 vms, centers = cluster_vms(vms, 3)
@@ -62,3 +105,6 @@ plt.ylabel("Memory (GB)")
 plt.title("VMs After K-means Clustering (3 Categories)")
 plt.colorbar(label="Category")
 plt.show()
+
+requests = generate_requests(time_steps=100, vms=vms, request_rate=0.3, avg_lifetime=5)
+deployment, dcp = initial_deployment(pms, vms)
