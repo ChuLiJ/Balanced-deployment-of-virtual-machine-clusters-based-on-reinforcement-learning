5a741d570e12e26458d8d7993b920379ac0b2b12
ChuLiJ <hrow@1241.com>
2025-03-22 13:36:41 +0800

    奖励函数

---
+++ reward.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+from scipy.stats import entropy
+
+from utils import normalize
+
+class Reward:
+
+    @classmethod
+    def nopms(cls, state):
+        nums_pm = 0
+        for pid in state:
+            if state[pid]:
+                nums_pm += 1
+        return nums_pm
+
+    @classmethod
+    def nom(cls, old_state, new_state):
+        migration_count = 0
+        for pid in old_state:
+            migration_count += (len(set(old_state[pid])) -
+                                len(set(old_state[pid]) & set(new_state.get(pid, []))))
+        return migration_count
+
+    @classmethod
+    def ei(cls, state, nums_category=3):
+        category_distribution = []
+        total_vms = sum(len(vm) for vm in state.value)
+        if total_vms == 0:
+            return 0.0
+        for pid, vm_list in state:
+            if not vm_list:
+                continue
+            category_count = [0] * nums_category
+            for vm in vm_list:
+                category_count[vm['Category']] += 1
+            dist = np.array(category_count) / len(vm_list)
+            category_distribution.append(dist)
+        global_dist = np.mean(category_distribution, axis=0) if category_distribution else np.zeros(nums_category)
+
+        js_divs = []
+        for dist in category_distribution:
+            m = 0.5 * (dist + global_dist)
+            js = 0.5 * (entropy(dist, m) + entropy(global_dist, m))
+            js_divs.append(js)
+
+        return -np.mean(js_divs)
+
+    @classmethod
+    def si(cls, state, pms):
+        used_cpu_list = []
+        used_mem_list = []
+        for pid, vm_list in state.items():
+            if not vm_list:
+                continue
+            pm = next(pm for pm in pms if pm['Pid'] == pid)
+            used_cpu = sum(vm['Cpu'] for vm in vm_list)
+            used_mem = sum(vm['Mem'] for vm in vm_list)
+            used_cpu_list.append(used_cpu / pm['Cpu'])
+            used_mem_list.append(used_mem / pm['Mem'])
+        if len(used_cpu_list) < 2:
+            return 0.0
+        return np.std(used_cpu_list) + np.std(used_mem_list)
+
+    @classmethod
+    def compute_reward(cls, old_state, new_state, pms, NOPMS_RANGE, NOM_RANGE, EI_RANGE, SI_RANGE):
+        nopms_norm = normalize(cls.nopms(new_state), *NOPMS_RANGE)
+        nom_norm = normalize(cls.nom(old_state, new_state), *NOM_RANGE)
+        ei_norm = normalize(cls.ei(new_state), *EI_RANGE)
+        si_norm = normalize(cls.si(new_state, pms), *SI_RANGE)
+        return -0.4 * nopms_norm - 0.3 * nom_norm + 0.2 * ei_norm - 0.1 * si_norm
